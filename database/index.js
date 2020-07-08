fe70986f4986b0727b6197fbb3f7abf10a4892cc
gcking <george.king823@gmail.com>
2020-07-08 15:38:27 -0700

    Edit spelling in database/index.js

--- database/index.js
+++ database/index.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const address = 'localhost:27017'; // formerl 172.17.0.3
+const address = 'localhost:27017'; // formerly 172.17.0.3
 
 mongoose.connect(`mongodb://${address}/carousel`, {
   useNewUrlParser: true});
