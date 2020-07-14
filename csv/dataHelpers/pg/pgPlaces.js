95da8d7a82108784648afcfb1513682b410ceeb5
gcking <george.king823@gmail.com>
2020-07-13 20:26:29 -0700

    Tweak pgPlaces.js pictureUrl

--- csv/dataHelpers/pg/pgPlaces.js
+++ csv/dataHelpers/pg/pgPlaces.js
@@ -9,78 +9,6 @@ var generateTitle = () => {
   return adj[Math.floor(Math.random()* adj.length)] + ' ' + noun[Math.floor(Math.random()* noun.length)];
 };
 
-// OG
-
-// const dataGen = (fileName, counterStart, numRecords, last) => {
-//   const writer = csvWriter();
-//   var counter = counterStart;
-//   numRecords = last ? numRecords + 1 : numRecords;
-//   writer.pipe(fs.createWriteStream(path.join(__dirname, '..', '..', 'data', 'pg', 'places', `${fileName}`)));
-//   for (let i = 0; i < numRecords; i++) {
-
-//     let numBeds = Math.floor(Math.random() * 4 + 1);
-//     let bed = numBeds === 1 ? 'bed' : 'beds';
-
-//     writer.write({
-//       placeId: counter++,
-//       title: generateTitle(),
-//       pictureUrl: 'https://loremflickr.com/320/240/house',
-//       zipCode: faker.address.zipCode(),
-//       roomType: `${numBeds} ${bed}`,
-//       numberBeds: numBeds,
-//       rating: (Math.random() * (5.00 - 1.00) + 1.00).toFixed(2),
-//       numberReviews: Math.floor(Math.random() * 100 + 1),
-//       hostPlus: Math.round(Math.random() * 1),
-//       superHost: Math.round(Math.random() * 1),
-//       price: faker.commerce.price(),
-//       placeUrl: faker.internet.url(),
-//       streetAddress: faker.address.streetAddress(),
-//       phoneNumber: faker.phone.phoneNumber(),
-//       descr: faker.lorem.paragraph(),
-//       userName: faker.internet.userName(),
-//       firstName: faker.name.firstName(),
-//       lastName: faker.name.lastName(),
-//       email: faker.internet.email()
-//     });
-//   }
-//   writer.end();
-//   console.log(`${fileName}: done`);
-// };
-
-
-// var generateFiles = (records) => {
-//   var recordsPerFile = Math.floor(records/3);
-//   const file1Start = 1;
-//   const file2Start = file1Start + recordsPerFile;
-//   const file3Start = file2Start + recordsPerFile;
-
-//   dataGen('pgPlacesData1.csv', file1Start, recordsPerFile);
-//   dataGen('pgPlacesData2.csv', file2Start, recordsPerFile);
-//   dataGen('pgPlacesData3.csv', file3Start, recordsPerFile, true);
-// }
-
-
-
-// ALT
-
-// const writeUsers = fs.createWriteStream(path.join(__dirname, '..', '..', 'data', 'pg', 'places', 'pgPlacesData1.csv'));
-
-// const dataGen = () => {
-//   let i = 10000000;
-//   let placeId = 1;
-
-// };
-
-
-// var generateFiles = (records) => {
-
-// }
-
-
-
-// OG refactor
-
-
 const dataGen = (fileName, counterStart, numRecords, last) => {
   const writer = fs.createWriteStream(path.join(__dirname, '..', '..', 'data', 'pg', 'places', `${fileName}`));
   // numRecords + 1 adjusts for record lost to rounding down
@@ -97,7 +25,7 @@ const dataGen = (fileName, counterStart, numRecords, last) => {
       let bed = numBeds === 1 ? 'bed' : 'beds';
 
       let title = generateTitle();
-      let pictureUrl = 'https = /loremflickr.com/320/240/house';
+      let pictureUrl = 'https://loremflickr.com/320/240/house';
       let zipCode = faker.address.zipCode();
       let roomType = `${numBeds} ${bed}`;
       let numberBeds = numBeds;
