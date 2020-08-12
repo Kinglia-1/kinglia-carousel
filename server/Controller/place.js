44e9fbf1fbd69e966058b62579a8ad50d3746e28
gcking <george.king823@gmail.com>
2020-08-12 12:04:48 -0700

    Remove redis temporarily

--- server/Controller/place.js
+++ server/Controller/place.js
@@ -7,24 +7,52 @@ client.on('error', err => console.log('Error: ' + err));
 
 module.exports = {
   get: (req, res) => {
-    const { zip } = req.params;
-    return client.get(zip, (err, data) => {
-      if (data) {
-        res.status(200).send(data)
-      } else {
-        let text = `SELECT placeid, title, pictureurl, zipcode, roomtype, numberbeds, rating, numberreviews, hostplus, superhost, price, placeurl FROM places WHERE zipcode = '${req.params.zip}' LIMIT 12;`;
+    let text = `SELECT placeid, title, pictureurl, zipcode, roomtype, numberbeds, rating, numberreviews, hostplus, superhost, price, placeurl FROM places WHERE zipcode = '${req.params.zip}' LIMIT 12;`;
 
-        pg.query(text)
-        .then((data) => {
-          // make sure data type works for Redis
-          client.setex(zip, 7200, JSON.stringify(data.rows));
-          res.status(200).send(data.rows);
-        })
-        .catch((err) => {
-          console.log('error in Places GET: ' + err);
-          res.sendStatus(400);
-        });
-      }
+    pg.query(text)
+    .then((data) => {
+      res.status(200).send(data.rows);
     })
+    .catch((err) => {
+      console.log('error in Places GET: ' + err);
+      res.sendStatus(400);
+    });
   }
 };
+
+// without redis
+get: (req, res) => {
+  let text = `SELECT placeid, title, pictureurl, zipcode, roomtype, numberbeds, rating, numberreviews, hostplus, superhost, price, placeurl FROM places WHERE zipcode = '${req.params.zip}' LIMIT 12;`;
+
+  pg.query(text)
+  .then((data) => {
+    res.status(200).send(data.rows);
+  })
+  .catch((err) => {
+    console.log('error in Places GET: ' + err);
+    res.sendStatus(400);
+  });
+}
+
+// with redis
+// get: (req, res) => {
+//   const { zip } = req.params;
+//   return client.get(zip, (err, data) => {
+//     if (data) {
+//       res.status(200).send(data)
+//     } else {
+//       let text = `SELECT placeid, title, pictureurl, zipcode, roomtype, numberbeds, rating, numberreviews, hostplus, superhost, price, placeurl FROM places WHERE zipcode = '${req.params.zip}' LIMIT 12;`;
+
+//       pg.query(text)
+//       .then((data) => {
+//         // make sure data type works for Redis
+//         client.setex(zip, 7200, JSON.stringify(data.rows));
+//         res.status(200).send(data.rows);
+//       })
+//       .catch((err) => {
+//         console.log('error in Places GET: ' + err);
+//         res.sendStatus(400);
+//       });
+//     }
+//   })
+// }
